6ed7d8f2c1c8be74432fed2b4bc5ad0c9d502520
Shekhar Kumar <sk164949@gmail.com>
2024-01-09 22:25:38 +0530

    1stprojectcompleted

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+
+
+const App = () => {
+  return (
+    <div>
+    <Navbar/>
+    <Hero/>
+    </div>
+  );
+};
+
+export default App;
