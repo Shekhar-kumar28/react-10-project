c385cf828b7a3d3e1276923b4365994cffa39d12
Shekhar Kumar <sk164949@gmail.com>
2024-01-10 18:14:30 +0530

    2nd project

---
+++ Project-2/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import ContactForm from './Components/ContactForm/ContactForm'
+import ContactHeader from './Components/ContactHeader/ContactHeader'
+import Navigation from './Components/Navigation/Navigation'
+
+function App() {
+
+  return (
+    <>
+    <Navigation/> 
+    <ContactHeader/>
+    <ContactForm/>
+    </>
+  )
+}
+
+export default App
